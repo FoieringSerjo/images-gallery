96c2bb390b863af9434c856a74e2eac4136f959e
FoieringSerjo <serjofoi@gmail.com>
2022-04-30 15:26:09 +0300

    enabled pylint and black code formatter in python

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from distutils.debug import DEBUG
+"""System module"""
 import os
 import requests
 from flask import Flask, request
@@ -14,7 +14,8 @@ DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
     raise EnvironmentError(
-        "Please create .env.local file and insert there UNSPLASH_KEY")
+        "Please create .env.local file and insert there UNSPLASH_KEY"
+    )
 
 app = Flask(__name__)
 CORS(app)
@@ -24,12 +25,10 @@ app.config["DEBUG"] = DEBUG
 
 @app.route("/new-image")
 def new_image():
+    """Due to pylint docstring"""
     word = request.args.get("query")
 
-    headers = {
-        "Accept-Version": "v1",
-        "Authorization": "Client-ID " + UNSPLASH_KEY
-    }
+    headers = {"Accept-Version": "v1", "Authorization": "Client-ID " + UNSPLASH_KEY}
     params = {"query": word}
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
 
